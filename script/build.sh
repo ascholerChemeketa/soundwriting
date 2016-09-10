866f5cc8cd3c5250c41fc2af612c98f99776c0a4
Rob Beezer <beezer@ups.edu>
2016-09-10 14:28:12 -0700

    Reorganize and extend build script

--- script/build.sh
+++ script/build.sh
@@ -19,39 +19,76 @@ declare MBXSL=${MBX}/xsl
 declare MBUSER=${MBX}/user
 declare SOURCE=${SRC}/src
 declare IMAGES=${SRC}/images
-# `ups-writers` in `mathbook/contrib`
-echo
-echo "BUILD: Update Custom XML :BUILD"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-install -d ${MBUSER}
-cp ${MBX}/contrib/ups-writers/ups-writers-html.xsl ${MBUSER}/ups-writers-html.xsl
-cp ${MBX}/contrib/ups-writers/ups-writers-latex.xsl ${MBUSER}/ups-writers-latex.xsl
 
-# Create directories
-echo
-echo "BUILD: Setup Scratch Directories :BUILD"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-install -d ${SCRATCH} ${SCRATCH}/pdf ${SCRATCH}/html ${SCRATCH}/pdf/images ${SCRATCH}/html/images
-install -d ${SCRATCH}/pdf ${SCRATCH}/pdf/images
-install -d ${SCRATCH}/html ${SCRATCH}/html/images
+# Common setup
+function setup {
+    # not necessary, but always build scratch directory first
+    echo
+    echo "BUILD: Setup Scratch Directory :BUILD"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    install -d ${SCRATCH}
 
-# Build the HTML Version
-echo
-echo "BUILD: Building HTML Version :BUILD"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cd ${SCRATCH}/html
-rm *.html
-rm -rf knowl
-cp -a ${IMAGES}/* ./images/
-xsltproc --xinclude ${MBUSER}/ups-writers-html.xsl ${SOURCE}/WritersHandbook.mbx
+    # Always place/update `ups-writers` in `mathbook/contrib`
+    echo
+    echo "BUILD: Update Custom XML :BUILD"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    install -d ${MBUSER}
+    cp ${MBX}/contrib/ups-writers/ups-writers-html.xsl ${MBUSER}/ups-writers-html.xsl
+    cp ${MBX}/contrib/ups-writers/ups-writers-latex.xsl ${MBUSER}/ups-writers-latex.xsl
+}
 
-# Build the PDF/print Version
-echo
-echo "BUILD: Building Print Version :BUILD"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cd ${SCRATCH}/pdf
-rm WritersHandbook.tex
-cp -a ${IMAGES}/* ./images/
-xsltproc --xinclude ${MBUSER}/ups-writers-latex.xsl ${SOURCE}/WritersHandbook.mbx
-xelatex WritersHandbook.tex
-xelatex WritersHandbook.tex
+# Subroutine to build the HTML Version
+function html_build {
+    echo
+    echo "BUILD: Building HTML Version :BUILD"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    install -d ${SCRATCH}/html ${SCRATCH}/html/images
+    cd ${SCRATCH}/html
+    rm *.html
+    rm -rf knowl
+    cp -a ${IMAGES}/* ./images/
+    xsltproc --xinclude ${MBUSER}/ups-writers-html.xsl ${SOURCE}/WritersHandbook.mbx
+}
+
+function view_html {
+    ${HTMLVIEWER} ${SCRATCH}/html/index.html
+}
+
+# Subroutine to build the PDF/print Version
+function pdf_build {
+    echo
+    echo "BUILD: Building Print Version :BUILD"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    install -d ${SCRATCH}/pdf ${SCRATCH}/pdf/images
+    cd ${SCRATCH}/pdf
+    rm WritersHandbook.tex
+    cp -a ${IMAGES}/* ./images/
+    xsltproc --xinclude ${MBUSER}/ups-writers-latex.xsl ${SOURCE}/WritersHandbook.mbx
+    xelatex WritersHandbook.tex
+    xelatex WritersHandbook.tex
+}
+
+function view_pdf {
+    ${PDFVIEWER} ${SCRATCH}/pdf/WritersHandbook.pdf
+}
+
+# Main command-line interpreter
+case "$1" in
+    "pdf")
+    setup
+    pdf_build
+    ;;
+    "html")
+    setup
+    html_build
+    ;;
+    "viewpdf")
+    view_pdf
+    ;;
+    "viewhtml")
+    view_html
+    ;;
+    *)
+    echo "Supply an option: pdf|html|viewpdf|viewhtml"
+    ;;
+esac
